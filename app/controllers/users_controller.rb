aea7e704e09b71d66969e12fe1b46397562c1f1a
wicifer <wictorfts@gmail.com>
2022-05-15 23:21:40 -0300

    pagination and small layout changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Account successfully updated."
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
@@ -30,11 +30,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 3)
   end
 
   private
